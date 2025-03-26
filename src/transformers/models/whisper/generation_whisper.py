93a573bdb73a3a157db6dc7d0a22074ed8dfb592
af-ai-group <F_Group_AI@appsfactory.de>
2025-03-26 16:17:29 +0100

    [FIX] tensor size mismatch issue

--- src/transformers/models/whisper/generation_whisper.py
+++ src/transformers/models/whisper/generation_whisper.py
@@ -313,7 +313,10 @@ class WhisperGenerationMixin:
                 # since jump_times is only extended in the next step, take index_ and not (index_ + 1)
                 next_element = jump_times[index_] if index_ < len(jump_times) else round((time_indices[-1] * time_precision), 4)
                 jump_times.insert(index_, next_element)
-            timestamps[batch_idx, 1:] = torch.tensor(jump_times)
+            if timestamps.shape[-1] == len(jump_times):
+                timestamps[batch_idx, 0:] = torch.tensor(jump_times)
+            else:
+                timestamps[batch_idx, 1:] = torch.tensor(jump_times)
 
         return timestamps
 
